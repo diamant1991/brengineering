359f4f5102ccf9903ddaa9693692095b1c348d7f
diamant1991 <diamant1988@mail.ru>
2017-10-20 14:33:48 +0300

    add files

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+$('.dropdown-btn').click(function(e) {
+  e.preventDefault()
+  var menu = $(this).next()
+  if(menu.is(":hidden")){
+    $(this).addClass('active')
+    menu.slideDown(250)
+  }
+  else{
+    $(this).removeClass('active')
+    menu.slideUp(250)
+  }
+});
---
+++ src/js/vendor.js
@@ -0,0 +1,4 @@
+//= ../../bower_components/jquery/dist/jquery.min.js
+//= ../../bower_components/bootstrap/js/transition.js
+//= ../../bower_components/bootstrap/js/modal.js
+//= ../../bower_components/bootstrap/js/tab.js
